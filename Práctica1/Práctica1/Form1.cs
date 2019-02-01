4c6cbfb6dab109afb44666f82d55a74462d06d20
VictorMCruzB <43403238+VictorMCruzB@users.noreply.github.com>
2019-02-01 11:41:17 -0800

    Unidad 1 TAP

---
+++ Práctica1/Práctica1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Práctica1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int res = int.Parse(textBox1.Text) + int.Parse(textBox2.Text);
+            textBox3.Text = res.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.textBox3.Visible = true;
+        }
+    }
+}
